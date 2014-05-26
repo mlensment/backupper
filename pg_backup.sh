b3d7091125931a66bcf9117d56a4f50c9dad517b
Martin Lensment <mlensment@gmail.com>
2014-05-26 10:46:01 +0300

    Simple dump script

--- pg_backup.sh
+++ pg_backup.sh
@@ -1,32 +1,36 @@
 #!/bin/bash
 
-if [ $# = 0 ]; then
-  SCRIPTPATH=$(cd ${0%/*} && pwd -P)
-  source $SCRIPTPATH/pg_backup.config
-fi;
 
-if ! touch $SCRIPTPATH/pg_backup.log; then
-  log "[!!ERROR!!] Cannot update backupper log file. Exiting."
-  exit 1;
-fi;
+######################
+## BACKUPPER CONFIG ##
+######################
+
+# Optional username to connect to database as.  Will default to "postgres" if none specified.
+HOSTNAME=localhost
+
+USERNAME=blog_v4
+
+#Password
+PGPASSWORD=blog_v4_pwd
+
+# Schema
+SCHEMA=public
+
+# Database
+DATABASE=blog_v4_development
+
+### SCRIPT ###
 
 log() {
   echo -e $1
-  echo -e $1 >> $SCRIPTPATH/pg_backup.log
 }
-
-log_from_tmp() {
-  cat tmp.txt >> $SCRIPTPATH/pg_backup.log
-}
-
 
 backup_to_local() {
   log "Backupping to local"
 
-  pg_dump -Fp -h "$HOSTNAME" -U "$USERNAME" -n "$SCHEMA" "$DATABASE" 2> tmp.txt | gzip > "$FINAL_BACKUP_FILE".sql.gz.in_progress
+  pg_dump -Fp -h "$HOSTNAME" -U "$USERNAME" -n "$SCHEMA" "$DATABASE" 2> log.txt | gzip > "$FINAL_BACKUP_FILE".sql.gz.in_progress
 
   if [ ${PIPESTATUS[0]} -ne "0" ]; then
-    log_from_tmp
     log "\n[!!ERROR!!] Failed to backup database $DATABASE. Exiting."
     rm  "$FINAL_BACKUP_FILE".sql.gz.in_progress
     exit 1;
@@ -34,20 +38,6 @@ backup_to_local() {
 
   mv "$FINAL_BACKUP_FILE".sql.gz.in_progress "$FINAL_BACKUP_FILE".sql.gz
   log "Backupped to local $FINAL_BACKUP_FILE.sql.gz"
-}
-
-delete_local_backups() {
-  # Delete old backups
-  log "Deleting local backups older than $DAYS_TO_KEEP days"
-  for f in $(find $BACKUP_DIR/* -mtime +$DAYS_TO_KEEP)
-  do
-    log "Deleting file $f from local"
-    rm $f
-  done
-}
-
-delete_backups() {
-  delete_local_backups
 }
 
 TIME=`date +\%Y-\%m-\%d_\%H-\%M`
@@ -58,18 +48,6 @@ log "Selected schema: $SCHEMA"
 
 export PGPASSWORD="$PGPASSWORD"
 
-# Make sure we're running as the required backup user
-if [ "$BACKUP_USER" != "" -a "$(id -un)" != "$BACKUP_USER" ]; then
-  log "[!!ERROR!!] This script must be run as $BACKUP_USER. Exiting."
-  exit 1;
-fi;
-
-# Create output directory
-if ! mkdir -p $BACKUP_DIR; then
-  log "[!!ERROR!!] Cannot create backup directory $BACKUP_DIR. Exiting."
-  exit 1;
-fi;
-
 # Set defaults
 if [ ! $HOSTNAME ]; then
   HOSTNAME="localhost"
@@ -79,12 +57,10 @@ if [ ! $USERNAME ]; then
   USERNAME="postgres"
 fi;
 
-FINAL_BACKUP_FILE="$BACKUP_DIR"/"$TIME"
+FINAL_BACKUP_FILE="./"$TIME""
 
 # Perform backup
 backup_to_local
-delete_backups
 
-rm tmp.txt
-
+rm log.txt
 log "Backup complete\n"
